92fb43411fbbfc2494b55fd673ff439aeb384438
Samuel Parrales <samuel.parrales@outlook.com>
2024-02-04 23:24:49 -0500

    base url production

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import react from '@vitejs/plugin-react'
 const base = process.env.NODE_ENV == 'development'
               ?''
               :'/react-news-app/'
+
 export default defineConfig({
   plugins: [react()],
+  base,
 })
